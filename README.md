1dfa18283c13cf20fb31ad8ce1bceda35ac7025a
Jonatan Kłosko <jonatanklosko@gmail.com>
2021-03-24 13:57:16 +0100

    Replace MathJax with KaTex

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # ExDoc with LaTeX
 
 Here you can find an example of rendering LaTeX expressions
- using [MathJax](https://www.mathjax.org/) in documentation generated with ExDoc.
+ using [KaTeX](https://katex.org/) in documentation generated with ExDoc.
 See [mix.exs](./mix.exs) for more details.
 
 ![](img/ex_doc_latex.png)
--- mix.exs
+++ mix.exs
@@ -37,21 +37,26 @@ defmodule LatexExample.MixProject do
     ]
   end
 
-  # In our case we simply add a <script> tag
-  # that loads MathJax from CDN and specify the configuration.
-  # Once loaded, the script will dynamically turn any LaTeX
-  # expressions on the page into SVG images.
+  # In our case we simply add a tags to load KaTeX
+  # from CDN and then specify the configuration.
+  # Once loaded, the script will dynamically render all LaTeX
+  # expressions on the page in place.
+  # For more details and options see https://katex.org/docs/autorender.html
   defp before_closing_body_tag(:html) do
     """
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/katex.min.css" integrity="sha384-t5CR+zwDAROtph0PXGte6ia8heboACF9R5l/DiY+WZ3P2lxNgvJkQk5n7GPvLMYw" crossorigin="anonymous">
+    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/katex.min.js" integrity="sha384-FaFLTlohFghEIZkw6VGwmf9ISTubWAVYW8tG8+w2LAIftJEULZABrF9PPFv+tVkH" crossorigin="anonymous"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/contrib/auto-render.min.js" integrity="sha384-bHBqxz8fokvgoJ/sc17HODNxa42TlaEhB+w8ZJXTc2nZf1VgEaFZeZvT4Mznfz0v" crossorigin="anonymous"></script>
     <script>
-      window.MathJax = {
-        tex: {
-          inlineMath: [['$', '$']],
-          displayMath: [['$$','$$']],
-        },
-      };
+      document.addEventListener("DOMContentLoaded", function() {
+        renderMathInElement(document.body, {
+          delimiters: [
+            { left: "$$", right: "$$", display: true },
+            { left: "$", right: "$", display: false },
+          ]
+        });
+      });
     </script>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js"></script>
     """
   end
 
